b8937a657b4709336354ed5832c5ed72bb420ecb
Amit Gupta <amit.gupta.work12@gmail.com>
2024-09-28 23:40:00 +0530

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+// import React from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [menu, setmenu] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="Logo" className="logo" />
+
       <ul className="navbarMenu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li
+          onClick={() => setmenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          {" "}
+          home
+        </li>
+        <li
+          onClick={() => setmenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setmenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile app
+        </li>
+        <li
+          onClick={() => setmenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
 
       <div className="navbarRight">
         <img src={assets.search_icon} alt="search-icon" />
+
         <div className="navbarSearchIcon">
           <img src={assets.basket_icon} alt="basket-icon" />
           <div className="dot"></div>
         </div>
-
         <button>Sign In</button>
       </div>
     </div>
