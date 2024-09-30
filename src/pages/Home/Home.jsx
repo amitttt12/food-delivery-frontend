523749607a9aee9f2b030786e32a510d4d80e585
Amit Gupta <amit.gupta.work12@gmail.com>
2024-09-30 22:38:13 +0530

    added explore menu list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
-  return <div>
-    <Header/>
-  </div>;
+  const [category, setCategory] = useState("ALl");
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
 };
 
 export default Home;
